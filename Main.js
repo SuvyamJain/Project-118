f1fc0313a4be74cfe3afc06b3787a3b75ba4aeb7
SuvyamJain <84795180+SuvyamJain@users.noreply.github.com>
2022-02-14 22:45:45 +0530

    Add files via upload

---
+++ Main.js
@@ -0,0 +1,25 @@
+random_no = Math.floor((Math.random()*array_1.length)+1);
+timer_counter = 0;
+Score = 0;
+
+function setup()
+{
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    background("White")
+
+}
+
+function draw()
+{
+
+}
+
+function check_sketch() {
+
+    
+}
+function clearCanvas() 
+{
+    background("white")
+}
